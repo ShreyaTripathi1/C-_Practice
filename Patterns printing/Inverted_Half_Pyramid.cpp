005367dd5ca4d462c86cf34ba4863c2dc27e068e
SHREYA TRIPATHI <shreyatripathi1808@gmail.com>
2024-11-18 01:06:01 +0530

    Add files via upload

---
+++ Patterns printing/Inverted_Half_Pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    for(int i=x; i>=1; i--){
+        for (int j=i; j>=1; j--){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// *  *  *  *  * 
+//  *  *  *  * 
+//  *  *  *
+//  *  *
+//  *
